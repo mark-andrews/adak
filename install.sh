dc4985d108f99a29095d713a44c0183360eab6b1
Mark Andrews <mjandrews.org@gmail.com>
2023-07-25 13:41:58 +0100

    Add new installation script using pip requirements

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VENV=$HOME/var/venvirons/psychopy2
+VENV=$HOME/var/venvirons/psychopy2c
 
 if [ -d $VENV ]; then
 	rm -rf $VENV
@@ -10,18 +10,4 @@ virtualenv $VENV
 
 source $VENV/bin/activate
 
-pip install --upgrade pip
-pip install attrdict3
-pip install wxpython
-pip install numpy scipy jupyter pandas matplotlib
-pip install pyyaml
-pip install tables
-pip install 'requests[security]'
-pip install 'pyglet==1.5.10'
-pip install arabic_reshaper
-pip install freetype-py
-pip install psutil
-pip install json_tricks
-pip install python-bidi
-pip install psychopy --no-deps
-
+pip install -r requirements.txt
