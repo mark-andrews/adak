6bdc2f2ab5d8b31f77c3b750d023decc40d8cdb5
Mark Andrews <mjandrews.org@gmail.com>
2023-07-27 09:20:11 +0100

    Update process_results
    Modify the code to process another results json file.

--- process_results.R
+++ process_results.R
@@ -17,8 +17,11 @@ process_each_block <- function(i){
     mutate(block = results$block,
            type = results$type,
            participant = experiment_info$participant_id,
+           gender = experiment_info$participant_gender,
+           age = experiment_info$participant_age,
+           handedness = experiment_info$participant_handedness,
            datetime = experiment_info$datetime) %>%
-    relocate(participant, datetime, block, type)
+    relocate(participant, gender, age, handedness, datetime, block, type)
 }
 
 # all the data from the above json file as a data frame
@@ -26,4 +29,6 @@ data_df <-
   map_dfr(seq(2, length(results)), process_each_block) %>%
   mutate(left_larger = left_size > right_size, 
          left_press = key_pressed == 'left', 
-         accuracy = left_larger == left_press) 
+         accuracy = left_larger == left_press) %>% 
+  select(-c(left_larger, left_press, rt_time)) %>% 
+  rename(rt = rt_clock)
